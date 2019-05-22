e31788491fdf1f7c5353ed37aaf92f2ddb4aaee2
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-21 21:16:06 -0600

    Adds password fields to user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     user_name { Faker::Movies::StarWars.specie }
+    password { "password" }
+    password_confirmation { "password"}
   end
 end

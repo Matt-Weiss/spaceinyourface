61ca78ddf02402cd8453bdecb541fe50ce85abb4
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-21 20:43:55 -0600

    Creates migration for user table - dropped previous table due to db type switch

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_230132) do
+ActiveRecord::Schema.define(version: 2019_05_22_021940) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name"
     t.string "email"
+    t.string "user_name"
     t.string "password_digest"
-    t.string "google_token"
+    t.integer "google_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

252670cfdb969d9388c374b8583e8e5f50599c0d
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-22 18:06:27 -0600

    Adds routes for OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get '/o/oauth2/auth', to: 'sessions#create'
+  get 'auth/google_oauth2/callback', to: 'sessions#create'
+
 end

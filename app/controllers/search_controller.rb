ae1f0aeea7c05290f7fbc7e25a11ec6cd6b3d299
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-23 21:12:00 -0600

    Adds search controller, routes, and views - enables random page testing for logging out

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,9 @@
+class SearchController < ApplicationController
+
+  def new
+  end
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :users, only: [:new, :create]
+  resources :search, only: [:new, :index]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'

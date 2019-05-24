9773fc8d88abbec69b2d97a4b9f48e0b323b577d
Deonte Cooper <deonte.cooper12@gmail.com>
2019-05-24 17:58:21 -0600

    Files added while running rails g models

---
+++ spec/factories/zip_codes.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :zipcode do
+    zipcode { 80015 }
+    lat { "39.62731" }
+    lng { "-104.779084" }
+  end
+end
---
+++ spec/models/zip_code_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Zipcode, type: :model do
+
+end

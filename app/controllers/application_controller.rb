cf4877b3b10460f0f9420acd793abd193b9fdd94
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-25 14:16:17 -0600

    Creates CelestialBodies model, table, seeds, and helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user,
+                :celestial_bodies
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def celestial_bodies
+    @celestial_bodies ||= CelestialBodies.all
+  end
+
 end

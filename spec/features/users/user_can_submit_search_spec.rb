6be1f049b68ceb20c80c1656d6bf8cadf80a39b8
Corey Sheesley <corey.sheesley@gmail.com>
2019-05-25 18:33:11 -0600

    Adds updated search spec

--- spec/features/users/user_can_submit_search_spec.rb
+++ spec/features/users/user_can_submit_search_spec.rb
@@ -4,16 +4,24 @@ describe 'User can submit a search request' do
   context 'When I click on the "Search" button' do
     it 'I go to a search form, where I can select the bodies to search for' do
       user = create(:user)
+      moon = CelestialBodies.create(name: "Moon")
+      mars = CelestialBodies.create(name: "Mars")
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
       visit root_path
 
       click_on "New Search"
       expect(current_path).to eq(new_search_path)
-      
-      save_and_open_page
 
+      find(:css, "#bodies_[value='Moon']").set(true)
+      find(:css, "#bodies_[value='Mars']").set(true)
+
+      fill_in 'Zip Code', with: 80202
+
+      click_button "Search"
+
+      expect(current_url).to include("bodies[]=Moon&bodies[]=Mars&zip_code=80202")
+      expect(current_path).to eq(search_index_path)
     end
   end
 end

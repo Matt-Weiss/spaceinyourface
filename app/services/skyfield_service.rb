bd2a1cb7983f1c3ccff64c400bbd2e6f2072812e
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-26 21:37:47 -0600

    completes skyfield service

--- app/services/skyfield_service.rb
+++ app/services/skyfield_service.rb
@@ -1,3 +1,36 @@
 class SkyfieldService
+  def initialize(coordinates, bodies)
+    @coordinates = coordinates
+    @bodies = bodies
+  end
 
+  def body_data
+    response = conn.get
+    JSON.parse(response.body)
+  end
+
+  private
+
+    def conn
+      Faraday.new("https://skyfield-json.herokuapp.com/ephemerides") do |f|
+        f.params[:latitude] = latitude
+        f.params[:longitude] = longitude
+        f.params[:bodies] = body_params
+        f.adapter Faraday.default_adapter
+      end
+    end
+
+    def longitude
+      @coordinates.first
+    end
+
+    def latitude
+      @coordinates.last
+    end
+
+    def body_params
+      p = ""
+      @bodies.each { |body| p.concat("#{body},") }
+      p.chomp
+    end
 end

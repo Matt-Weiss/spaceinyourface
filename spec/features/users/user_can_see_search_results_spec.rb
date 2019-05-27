78fbb650189d244228e46d8562dc11645ab4f5a5
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-26 23:37:34 -0600

    finalizes search index spec

--- spec/features/users/user_can_see_search_results_spec.rb
+++ spec/features/users/user_can_see_search_results_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'User can see search results' do
   before :each do
     user = create(:user)
-    moon = CelestialBodies.create(name: "Moon")
+    moon = CelestialBodies.create(name: "Luna")
     mars = CelestialBodies.create(name: "Mars")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -17,7 +17,11 @@ describe 'User can see search results' do
     stub_request(:get, "https://api.mapbox.com/geocoding/v5/mapbox.places/1331%2017th%20st%20denver,%20co.json?access_token=#{ENV['MAPBOX_API_KEY']}")
       .to_return(status: 200, body: json_mapbox_response)
 
-    find(:css, "#bodies_[value='Moon']").set(true)
+    json_search_index_response = File.open('./spec/fixtures/search_data.json')
+    stub_request(:get, "https://skyfield-json.herokuapp.com/ephemerides?bodies=luna,mars&latitude=39.750772_N&longitude=104.996446_W")
+      .to_return(status: 200, body: json_search_index_response)
+
+    find(:css, "#bodies_[value='Luna']").set(true)
     find(:css, "#bodies_[value='Mars']").set(true)
 
     location = '1331 17th St Denver, CO'
@@ -27,11 +31,12 @@ describe 'User can see search results' do
     click_button "Search"
 
     expect(current_path).to eq(search_index_path)
-    expect(page).to have_content("My Location: #{location}")
-    expect(page).to have_css("#body", count: 2)
+    expect(page).to have_content("My Location: 1331 17th Street, Denver, Colorado 80202, United States")
+    save_and_open_page
+    expect(page).to have_css(".body", count: 2)
 
-    within first "#body" do
-      expect(page).to have_link("Moon")
+    within first ".body" do
+      expect(page).to have_link("Luna")
       expect(page).to have_content("Azimuth")
       expect(page).to have_content("Elevation")
       expect(page).to have_content("Right Ascension")

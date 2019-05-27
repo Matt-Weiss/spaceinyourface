4bdbe0156f5ad376062c8bf974a6390b6a65ed28
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-27 00:39:02 -0600

    adds description column to celestial_bodies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_201107) do
+ActiveRecord::Schema.define(version: 2019_05_27_063500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "celestial_bodies", force: :cascade do |t|
     t.string "name"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|

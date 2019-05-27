51d34268884c80e68fb9db68392953d6cf52af1c
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-26 21:09:11 -0600

    creates search_results_facade to pass to search index view

--- app/facades/search_results_facade.rb
+++ app/facades/search_results_facade.rb
@@ -1,8 +1,29 @@
 class SearchResultsFacade
-  attr_reader :location,
-              :bodies
+
   def initialize(search_params)
-    @location = search_params['location']
+    @raw_location = search_params['location']
     @bodies = search_params['bodies']
+  end
+
+  def user_estimated_location
+    mapbox_best_guess["place_name"]
+  end
+
+  def body_results
+    SkyfieldService.new(mapbox_long_lat, @bodies)
+  end
+
+  private
+
+  def location
+    @_location ||= MapboxService.new(@raw_location)
+  end
+
+  def mapbox_long_lat
+    mapbox_best_guess["center"]
+  end
+
+  def mapbox_best_guess
+    location.returned_locations["features"].first
   end
 end

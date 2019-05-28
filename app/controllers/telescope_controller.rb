a6aa9dbd126655e0fe7c2f3e9d41908fae539393
Matt Weiss <weiss.matt@outlook.com>
2019-05-28 14:25:52 -0600

    Makes controller consistent with params input from show page
    Co-authored-by: Jeremy Bennett 
    <25406708+JaxJafinPapau@users.noreply.github.com>

--- app/controllers/telescope_controller.rb
+++ app/controllers/telescope_controller.rb
@@ -11,7 +11,7 @@ class TelescopeController < ApplicationController
 
     motor = TelescopeMove.new(ephemeris_data)
 
-    scope_movement = Faraday.get("http://#{params[:ngrok_url]}/scopetrack?locate_object_ra_steps=#{motor.locate_ra_steps}&locate_object_dec_steps=#{motor.locate_dec_steps}&track_dec_delay=#{motor.track_dec_delay}&track_dec_steps=#{motor.track_dec_steps}&track_ra_delay=#{motor.track_ra_delay}&track_ra_steps=#{motor.track_ra_steps}&return_home_dec_steps=#{motor.total_steps[:dec_steps]}&return_home_ra_steps=#{motor.total_steps[:ra_steps]}")
+    scope_movement = Faraday.get("http://#{params[:ngrok_url]}/scopetrack?move_to_start_ra=#{motor.move_to_start_ra}&move_to_start_dec=#{motor.move_to_start_dec}&track_dec_delay=#{motor.track_dec_delay}&track_dec_steps=#{motor.track_dec_steps}&track_ra_delay=#{motor.track_ra_delay}&track_ra_steps=#{motor.track_ra_steps}&return_home_dec_steps=#{motor.total_steps[:dec_steps]}&return_home_ra_steps=#{motor.total_steps[:ra_steps]}")
     flash[:message] = "Your telescope has now started tracking #{body_name}!"
     #redirect_to #something show page
   end

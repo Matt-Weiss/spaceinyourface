5b45df53336956e39ea9330901f1eda46990aa45
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-29 00:05:24 -0600

    Refactors telescope_controller to expect api call from an Ngrok service.
    Co-authored-by: Matt Weiss 
    <45211960+Matt-Weiss@users.noreply.github.com>

--- app/controllers/telescope_controller.rb
+++ app/controllers/telescope_controller.rb
@@ -1,18 +1,24 @@
 class TelescopeController < ApplicationController
   def create
-    body = CelestialBodies.find(params[:celestial_body_id])
-    body_name = body.name.downcase
-    coordinates_array = params[:telescope][:location].split(",")
-
-    latitude = coordinates_array[0].to_s.gsub(" ", "_")
-    longitude = coordinates_array[1].to_s.gsub(" ", "_")
-
-    ephemeris_data = Faraday.get("https://skyfield-json.herokuapp.com/telescope_tracking?latitude=#{latitude}&longitude=#{longitude}&body=#{body_name}")
-
-    motor = TelescopeMove.new(ephemeris_data)
-
-    scope_movement = Faraday.get("http://#{params[:ngrok_url]}/scopetrack?move_to_start_ra=#{motor.move_to_start_ra}&move_to_start_dec=#{motor.move_to_start_dec}&track_dec_delay=#{motor.track_dec_delay}&track_dec_steps=#{motor.track_dec_steps}&track_ra_delay=#{motor.track_ra_delay}&track_ra_steps=#{motor.track_ra_steps}&return_home_dec_steps=#{motor.total_steps[:dec_steps]}&return_home_ra_steps=#{motor.total_steps[:ra_steps]}")
-    flash[:message] = "Your telescope has now started tracking #{body_name}!"
-    #redirect_to #something show page
+    ephemeris_data = SkyfieldService.new(user_coordinates, body_name, "telescope_tracking")
+    NgrokService.new(params[:ngrok_url], ephemeris_data)
+    flash[:message] = "Your telescope has now started tracking #{body_name[0]}!"
+    redirect_to celestial_body_path(body.name, location: user_coordinates)
   end
+
+  private
+
+    def body
+      CelestialBodies.find(params[:celestial_body_id])
+    end
+
+    def body_name
+      # SkyfieldSerivce is expecting an array of body names
+      [body.name.downcase]
+    end
+
+    def user_coordinates
+      # SkyfieldService is expecting the array to come in [long, lat]
+      params[:telescope][:location].split(",").reverse
+    end
 end

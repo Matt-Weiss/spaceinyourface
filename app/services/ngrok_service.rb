2f03a9d2a786c478b9a01cc96d596e9210a471db
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-29 21:57:32 -0600

    Takes line breaks out of conn method, makes motor an instance variable so all calls to the method saved to memory.
    Co-authored-by: Deonte Cooper <45864171+djc00p@users.noreply.github.com>
    Co-authored-by: Corey Sheesley 
    <43529041+CSheesley@users.noreply.github.com>
    Co-authored-by: Matt Levy <36040194+milevy1@users.noreply.github.com>
    Co-authored-by: Matt Weiss 
    <45211960+Matt-Weiss@users.noreply.github.com>

--- app/services/ngrok_service.rb
+++ app/services/ngrok_service.rb
@@ -2,21 +2,17 @@ class NgrokService
   def initialize(url, ephemeris_data)
     @url = url
     @ephemeris_data = ephemeris_data
+    @motor = motor
   end
 
-  def motor
-    TelescopeMove.new(@ephemeris_data)
-  end
 
   def conn
-    Faraday.get("http://#{@url}}/scopetrack?
-      move_to_start_ra=#{motor.move_to_start_ra}&
-      move_to_start_dec=#{motor.move_to_start_dec}&
-      track_dec_delay=#{motor.track_dec_delay}&
-      track_dec_steps=#{motor.track_dec_steps}&
-      track_ra_delay=#{motor.track_ra_delay}&
-      track_ra_steps=#{motor.track_ra_steps}&
-      return_home_dec_steps=#{motor.total_steps[:dec_steps]}&
-      return_home_ra_steps=#{motor.total_steps[:ra_steps]}")
+    Faraday.get("#{@url}move_to_start_ra=#{@motor.move_to_start_ra}&move_to_start_dec=#{@motor.move_to_start_dec}&track_dec_delay=#{@motor.track_dec_delay}&track_dec_steps=#{@motor.track_dec_steps}&track_ra_delay=#{@motor.track_ra_delay}&track_ra_steps=#{@motor.track_ra_steps}&return_home_dec_steps=#{@motor.total_steps['dec_steps']}&return_home_ra_steps=#{@motor.total_steps['ra_steps']}")
   end
+
+  private
+
+    def motor
+      TelescopeMove.new(@ephemeris_data.body_data)
+    end
 end

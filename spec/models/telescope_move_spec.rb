069fe239c8eae8d2da246de06a3c625dcf9fce33
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-29 23:11:27 -0600

    Changes symbolized hash keys to strings in telescope_move_spec
    Co-authored-by: Deonte Cooper <45864171+djc00p@users.noreply.github.com>
    Co-authored-by: Corey Sheesley 
    <43529041+CSheesley@users.noreply.github.com>
    Co-authored-by: Matt Levy <36040194+milevy1@users.noreply.github.com>
    Co-authored-by: Matt Weiss 
    <45211960+Matt-Weiss@users.noreply.github.com>

--- spec/models/telescope_move_spec.rb
+++ spec/models/telescope_move_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe 'telescope_move PORO' do
   describe 'instance methods' do
     before :each do
-      ephemeris_data = {"data":
-                   {"latitude": "39.7348 N",
-                   "longitude": "104.9653 W",
-                  "start_time": "2019-05-28 20:41:40",
-                    "end_time": "2019-05-28 21:41:40",
-              "celestial_body": "venus",
-                   "start_dec": "15.348037342090622",
-                    "start_ra": "2.9352524011268986",
-                     "end_dec": "15.363469874497973",
-                      "end_ra": "2.9385758993886246"}
+      ephemeris_data = {"data" =>
+                   {"latitude" => "39.7348 N",
+                   "longitude" => "104.9653 W",
+                  "start_time" => "2019-05-28 20:41:40",
+                    "end_time" => "2019-05-28 21:41:40",
+              "celestial_body" => "venus",
+                   "start_dec" => "15.348037342090622",
+                    "start_ra" => "2.9352524011268986",
+                     "end_dec" => "15.363469874497973",
+                      "end_ra" => "2.9385758993886246"}
                     }
       @motor = TelescopeMove.new(ephemeris_data)
     end
@@ -27,12 +27,12 @@ describe 'telescope_move PORO' do
 
     it 'move_to_start_dec' do
       expect(@motor.move_to_start_dec).to eq(2239)
-      expect(@motor.total_steps[:dec_steps]).to eq(2239)
+      expect(@motor.total_steps['dec_steps']).to eq(2239)
     end
 
     it 'move_to_start_ra' do
       expect(@motor.move_to_start_ra).to eq(-3522)
-      expect(@motor.total_steps[:ra_steps]).to eq(-3522)
+      expect(@motor.total_steps['ra_steps']).to eq(-3522)
     end
 
     it 'track_dec_steps' do

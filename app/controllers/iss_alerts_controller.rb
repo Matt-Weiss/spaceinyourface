b27e6d7e3e0fe9d2aa0399b058ea3dfba61b4bf2
Matt Levy <milevy1@gmail.com>
2019-06-05 11:36:24 -0600

    Updated formatting for text message input and response
    Co-authored-by: Corey Sheesley <43529041+CSheesley@users.noreply.github.com>

--- app/controllers/iss_alerts_controller.rb
+++ app/controllers/iss_alerts_controller.rb
@@ -1,14 +1,17 @@
 class IssAlertsController < ApplicationController
   def create
-    phone_number = '+1' + params['phone'].delete('-')
+    phone_number = '+1' + params['phone']
+    formatted_phone_number = params['phone'][0..2] + '-' + params['phone'][3..5] + '-' + params['phone'][6..9]
     scheduled_time = Time.parse(params['scheduled_time'])
 
     rise_time = scheduled_time + (15 * 60)
-    message = "The Int'l Space Station will be rising near you at #{rise_time}. Space In Your Face!"
+    formatted_rise_time = rise_time.strftime("%a %b%e %l:%M%p %Z")
+
+    message = "The International Space Station will be rising near you soon: #{formatted_rise_time}. SpaceInYourFace.herokuapp.com"
 
     alert = TextAlert.perform_at(scheduled_time, phone_number, message)
 
-    flash[:message] = "Text message successfully scheduled for #{params[:phone]} for next ISS rise time at #{scheduled_time}."
+    flash[:message] = "#{formatted_phone_number} - Text message successfully scheduled for next ISS rise time: #{formatted_rise_time}."
     redirect_to root_path
   end
 end

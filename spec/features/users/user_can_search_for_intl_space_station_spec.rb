2425513cee2c5013a57bf932a79f6b0ea05b65e4
Matt Weiss <weiss.matt@outlook.com>
2019-06-12 10:18:28 -0600

    Edits test to add mock for skyfield ping before search, changes path and field names to conform to new design.
    Co-authored-by: Corey Sheesley 
    <43529041+CSheesley@users.noreply.github.com>
    Co-authored-by: Jeremy Bennett 
    <25406708+JaxJafinPapau@users.noreply.github.com>
    
    Co-Authored-By: Deonte Cooper <45864171+djc00p@users.noreply.github.com>
    Co-Authored-By: Matt Levy <36040194+milevy1@users.noreply.github.com>

--- spec/features/users/user_can_search_for_intl_space_station_spec.rb
+++ spec/features/users/user_can_search_for_intl_space_station_spec.rb
@@ -10,17 +10,20 @@ describe 'Users can search for Intl space station' do
     stub_request(:get, "https://api.satellites.calum.org/rest/v1/25544/next-pass?alt=0&lat=39.750772&lon=-104.996446")
     .to_return(status: 200, body: json_iss_view_response)
 
+    stub_request(:get, "https://skyfield-json.herokuapp.com/")
+    .to_return(status: 200)
+
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit search_index_path
+    visit new_search_path
 
 
-      fill_in 'iss_location', with: '1331 17th St Denver, CO'
+      fill_in 'location', with: '1331 17th St Denver, CO'
       click_button "Find ISS"
 
 
     expect(current_url).to include("location=1331+17th+St+Denver%2C+CO")
-    expect(current_path).to eq(iss_search_index_path)
+    expect(current_path).to eq(search_index_path)
   end
 end

30957799ea87bae4f3dcd9b4e259321e868c39d8
Deonte Cooper <deonte.cooper12@gmail.com>
2019-06-19 11:19:49 -0600

    Added WebMock for DarkSky API to pass tests

--- spec/features/users/user_can_see_search_results_spec.rb
+++ spec/features/users/user_can_see_search_results_spec.rb
@@ -16,9 +16,14 @@ describe 'User can see search results' do
     visit new_search_path
 
     json_mapbox_response = File.open('./spec/fixtures/mapbox_data.json')
-
     stub_request(:get, "https://api.mapbox.com/geocoding/v5/mapbox.places/1331%2017th%20st%20denver,%20co.json?access_token=#{ENV['MAPBOX_API_KEY']}")
-      .to_return(status: 200, body: json_mapbox_response)
+    .to_return(status: 200, body: json_mapbox_response)
+
+
+    json_darksky_response = File.open('./spec/fixtures/darksky_data.json')
+    stub_request(:get, "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API']}/39.750772,-104.996446")
+      .to_return(status: 200, body: json_darksky_response)
+
 
     json_search_index_response = File.open('./spec/fixtures/search_data.json')
     stub_request(:get, "https://skyfield-json.herokuapp.com/ephemerides?bodies=luna,mars&latitude=39.750772_N&longitude=104.996446_W")
--- spec/features/users/user_can_visit_celestial_body_show_page_spec.rb
+++ spec/features/users/user_can_visit_celestial_body_show_page_spec.rb
@@ -19,6 +19,10 @@ describe 'As a registered user I can visit the celestial body show page' do
     json_search_index_response = File.open('./spec/fixtures/search_data.json')
     stub_request(:get, "https://skyfield-json.herokuapp.com/ephemerides?bodies=luna,mars&latitude=39.750772_N&longitude=104.996446_W")
       .to_return(status: 200, body: json_search_index_response)
+
+    json_darksky_response = File.open('./spec/fixtures/darksky_data.json')
+    stub_request(:get, "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API']}/39.750772,-104.996446")
+      .to_return(status: 200, body: json_darksky_response)
   end
 
   it "and see the body's name and a brief description" do

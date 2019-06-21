74ea1186dec4950c4010d5108d7ef04c154de51c
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-06-20 18:45:46 -0600

    Changes param expectations to fit with rise_time and location params now coming from the view. Moves all timing math to the controller. Calls on timezone gem to get a time zone for the searched location, then creates a message with that formatted time.

--- app/controllers/iss_alerts_controller.rb
+++ app/controllers/iss_alerts_controller.rb
@@ -2,16 +2,27 @@ class IssAlertsController < ApplicationController
   def create
     phone_number = '+1' + params['phone']
     formatted_phone_number = params['phone'][0..2] + '-' + params['phone'][3..5] + '-' + params['phone'][6..9]
-    scheduled_time = Time.parse(params['scheduled_time'])
+    location = formatted_location(params['location'])
+    time_zone = Timezone.lookup(location[1], location[0]).name
+    rise_time = Time.parse(params['rise_time'])
+    scheduled_time = rise_time - 15.minutes
+    formatted_rise_time = rise_time.in_time_zone(time_zone).strftime("%A, %b %e at%l:%M%p %Z")
 
-    rise_time = scheduled_time + (15 * 60)
-    formatted_rise_time = rise_time.strftime("%a %b%e %l:%M%p %Z")
-
-    message = "The International Space Station will be rising near you soon: #{formatted_rise_time}. SpaceInYourFace.herokuapp.com"
-
-    alert = TextAlert.perform_at(scheduled_time, phone_number, message)
+    alert = TextAlert.perform_at(scheduled_time, phone_number, message(formatted_rise_time))
 
     flash[:success] = "#{formatted_phone_number} - Text message successfully scheduled for next ISS rise time: #{formatted_rise_time}."
     redirect_to new_search_path
   end
+
+  private
+
+    def message(formatted_rise_time)
+      "The International Space Station will be rising near you soon: #{formatted_rise_time}. SpaceInYourFace.herokuapp.com"
+    end
+
+    def formatted_location(strf_location)
+      strf_location.split.map do |coord|
+        coord.to_f
+      end
+    end
 end
